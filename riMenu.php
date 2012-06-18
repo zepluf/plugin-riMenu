ff2d383790b908f24b2a8b1a5f389a1d3d6c5541
yellow1912 <yellow1912@gmail.com>
2012-06-18 14:42:28 +0700

    update installer

---
+++ riMenu.php
@@ -0,0 +1,18 @@
+<?php
+namespace plugins\riMenu;
+
+use plugins\riCore\PluginCore;
+use plugins\riPlugin\Plugin;
+
+class riMenu extends PluginCore{
+    
+    public function install(){
+        return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/sql/install.sql'));
+        return true;
+    }
+    
+    public function uninstall(){
+        return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/sql/uninstall.sql'));
+        return true;
+    }
+}

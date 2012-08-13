fbcffddf3c919d5a7083ff78a6eb349a2caa388e
KhanhTran88 <cnkhanhdl@gmail.com>
2012-08-13 11:17:11 +0700

    .

--- riMenu.php
+++ riMenu.php
@@ -8,17 +8,9 @@ class riMenu extends PluginCore{
     
     public function install(){
         return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/sql/install.sql'));
-<<<<<<< HEAD
-=======
-        return true;
->>>>>>> 70e9db6f62263854623e4d90341d692f9a874bff
     }
     
     public function uninstall(){
         return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/sql/uninstall.sql'));
-<<<<<<< HEAD
-=======
-        return true;
->>>>>>> 70e9db6f62263854623e4d90341d692f9a874bff
     }
 }
